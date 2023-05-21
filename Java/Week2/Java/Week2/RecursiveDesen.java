e67a39c1e9359c7ab8e5d526a8198fcf47c285b1
Hasan Avci <f201213030@ktun.edu.tr>
2023-05-21 18:52:19 +0300

    directory changed

--- Java/Week2/Java/Week2/RecursiveDesen.java
+++ Java/Week2/Java/Week2/RecursiveDesen.java
@@ -1,7 +1,11 @@
 package Java.Week2;
 
+import java.util.Scanner;
+
 public class RecursiveDesen {
     public static void main(String[] args) {
+        //this code is just for testing
+        //it will be modified soon
         System.out.println("Test 123");
     }
 }
