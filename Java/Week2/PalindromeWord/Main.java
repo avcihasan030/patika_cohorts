4a144b4dcdaf2bda0a876231ff8b759969bee2f5
Hasan Avci <f201213030@ktun.edu.tr>
2023-05-23 09:37:43 +0300

    document formatted

--- Java/Week2/PalindromeWord/Main.java
+++ Java/Week2/PalindromeWord/Main.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String _word;
+        try (Scanner scanner = new Scanner(System.in)) {
+            String _word;
 
-        System.out.print("Enter a word : ");
-        _word = scanner.nextLine();
+            System.out.print("Enter a word : ");
+            _word = scanner.nextLine();
 
-        System.out.println(isPalindrome(_word));
+            System.out.println(isPalindrome(_word));
+        }
     }
 
     private static String isPalindrome(String _word) {
-         String rev = "";
-         for (int i = _word.length()-1; i >= 0; i--) {
+        String rev = "";
+        for (int i = _word.length() - 1; i >= 0; i--) {
             rev += _word.charAt(i);
-         }
+        }
 
-         return (_word.equalsIgnoreCase(rev)) ? "This is palindrome":"This is not palindrome";
+        return (_word.equalsIgnoreCase(rev)) ? "This is palindrome" : "This is not palindrome";
     }
-    
+
 }
